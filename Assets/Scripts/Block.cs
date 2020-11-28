88b47ff698efb1712c62e5b1d87bee4f3e0f0e89
narumia0327 <73158256+narumia0327@users.noreply.github.com>
2020-11-29 01:44:24 +0900

    Add files via upload

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Block : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		GameObject textui = GameObject.Find("Message");
+		int score = int.Parse(textui.GetComponent<Text>().text);
+		score += 100;
+		textui.GetComponent<Text>().text = score.ToString();
+
+		Destroy(this.gameObject);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        this.transform.position -= Vector3.left * x * 0.1f;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Ball")
+        {
+            Vector3 v = other.rigidbody.velocity;
+            Vector3 ballv = Vector3.Normalize(new Vector3(v.x, 1, 1));
+            other.rigidbody.velocity = ballv * 50;
+        }
+    }
+}
